86d35dbadf819d99b640ee631592970ad8f45944
MainframeKuznetSov <mainframe.comp.prog@gmail.com>
2025-11-27 19:14:42 +0530

    Spring dependencies inserted

---
+++ blog_app/src/main/java/com/aditya/blog_app/BlogKraftApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.blog_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogKraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogKraftApplication.class, args);
+	}
+
+}
---
+++ blog_app/src/test/java/com/aditya/blog_app/BlogKraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.blog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogKraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
